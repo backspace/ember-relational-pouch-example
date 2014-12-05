afe65e085f2b5554d2ecdf8f4924e0b088b3a177
Buck Doyle <b@chromatin.ca>
2014-12-05 15:49:55 -0500

    Add button to display database contents

--- app/routes/application.js
+++ app/routes/application.js
@@ -10,6 +10,12 @@ export default Ember.Route.extend({
       this.store.find('feature');
     },
 
+    printDatabase: function() {
+      this.container.lookup('adapter:application').db.allDocs({include_docs: true}, function(err, docs) {
+        $('pre').html(JSON.stringify(docs, null, 4));
+      });
+    },
+
     clearDatabase: function() {
       this.container.lookup('adapter:application').db.destroy(function() {
         window.location.reload();
