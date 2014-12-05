a953cc12c1a6c75fe5b5a8457072ffdf72ab3f07
Buck Doyle <b@chromatin.ca>
2014-12-05 18:18:02 -0500

    Remove unnecessary inverse specification

--- app/models/issue.js
+++ app/models/issue.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  features: DS.hasMany('feature', {inverse: 'issue'}),
+  features: DS.hasMany('feature'),
 
   rev: DS.attr('string')
 });
