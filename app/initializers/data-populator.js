643d699ea6805407fc92351a36e6ac56d17a1b82
Buck Doyle <b@chromatin.ca>
2014-12-05 15:28:01 -0500

    Add fixture-like data

---
+++ app/initializers/data-populator.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'data-populator',
+  after: 'store',
+
+  initialize: function(container, application) {
+    application.deferReadiness();
+
+    var store = container.lookup('store:main');
+
+    var issues = store.find('issue');
+    issues.then(function() {
+      if (issues.get('length') == 0) {
+        var issue = store.createRecord('issue', {title: 'An issue'});
+
+        issue.save().then(function() {
+          var feature = store.createRecord('feature', {title: 'A feature'});
+          issue.get('features').pushObject(feature);
+          feature.save();
+          issue.save().then(function() {
+            application.advanceReadiness();
+          });
+        });
+      }
+      else {
+        application.advanceReadiness();
+      }
+    });
+  }
+}
